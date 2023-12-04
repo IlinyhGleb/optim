91f9b629037203a0ea62185cdcadc3ed2bff09bc
Gleb Ilinyh <gleb@ilinyh.ru>
2023-12-04 13:29:09 +0500

    изменил получение пределов для групп

--- json_server/app.py
+++ json_server/app.py
@@ -25,15 +25,22 @@ def get_optim_solu():
     cursor = connection.cursor()
 
     ref_id = 1
+    # cursor.execute(
+    #     'SELECT refrigerator_id, product_id, caloricity, amount, category_id '
+    #     'FROM refrigerator_has_product JOIN product '
+    #     'WHERE product_id = id AND refrigerator_id = ?',
+    #     (ref_id,))
+
+
     cursor.execute(
         'SELECT refrigerator_id, product_id, caloricity, category_id '
         'FROM refrigerator_has_product JOIN product '
         'WHERE product_id = id AND refrigerator_id = ?',
         (ref_id,))
     groups = cursor.fetchall()
-    for group in groups:
-        print(group)
-
+    # for group in groups:
+    #     print(group)
+    # print(len(groups))
     # калорийность продуктов
     food_energy_groups = []
     for i in range(1, 9):
@@ -52,6 +59,7 @@ def get_optim_solu():
         'WHERE product_id = id AND refrigerator_id = ?',
         (ref_id,))
     groups = cursor.fetchall()
+
     food_quantity_groups = []
     for i in range(1, 9):
         b = []
@@ -61,27 +69,12 @@ def get_optim_solu():
         food_quantity_groups.append(b)
 
     # ограничения групп
-    cursor.execute('SELECT min FROM limits')
-    l_min = cursor.fetchall()
-    limits_min = np.zeros(8)
-    k = 0
-    for x in l_min:
-        limits_min[k] = x[0]
-        k = k + 1
-    cursor.execute('SELECT max FROM limits')
-
-    l_max = cursor.fetchall()
-
+    cursor.execute('SELECT min, max FROM limits ORDER BY category_id')
+    limits = cursor.fetchall()
     connection.close()
 
-    limits_max = np.zeros(8)
-    k = 0
-    for x in l_max:
-        limits_max[k] = x[0]
-        k = k + 1
-
-    group_limits_min = np.array(limits_min)
-    group_limits_max = np.array(limits_max)
+    group_limits_min = np.array([sublist[0] for sublist in limits])
+    group_limits_max = np.array([sublist[1] for sublist in limits])
 
     food_energy_groups_array = np.array(flatten(food_energy_groups))
     food_limits_array = np.array(flatten(food_quantity_groups))
@@ -97,7 +90,8 @@ def get_optim_solu():
                      penalty=1e1, penalty_power=2)
     x0 = np.zeros(len(food_energy_groups_array))
 
-    (res, iter), time = nelder_mead(ff, x0, gamma=2, maxiter=20000, dx=100)
+    (res, iter), time = nelder_mead(ff, x0, gamma=2, maxiter=20000, dx=10)
+    print(iter)
     return jsonify({'result': res.tolist()})
 
 
